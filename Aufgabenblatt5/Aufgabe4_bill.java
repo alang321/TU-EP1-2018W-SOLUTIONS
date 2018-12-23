566863214a30e887a9e399c379bd745847b54306
bmdyy <45366012+bmdyy@users.noreply.github.com>
2018-12-23 02:08:12 +0100

    Update Aufgabe4_bill.java

--- Aufgabenblatt5/Aufgabe4_bill.java
+++ Aufgabenblatt5/Aufgabe4_bill.java
@@ -6,9 +6,6 @@ public class Aufgabe4 {
     //constant circle radius
     private static final int RADIUS = 10;
 
-    //no idea how to make printShape() without this. is it possible?
-    private static int valLow = 0;
-
     private static void printCirclesInLine(int val, int x, int y) {
         if (val == 0) return;
         StdDraw.setPenColor(val%2==0 ? StdDraw.ORANGE : StdDraw.BLUE);
@@ -18,11 +15,13 @@ public class Aufgabe4 {
     }
 
     private static void printShape(int val, int max, int y) {
-        if (y == RADIUS) valLow = val;
-        printCirclesInLine(val, RADIUS, y);
-        boolean b = y < (RADIUS + RADIUS * 4 * (max - valLow));
-        if (val == valLow && !b) return;
-        printShape(val + (b ? 1 : -1), max, y + RADIUS * 4);
+        if (val == max) { printCirclesInLine(val, RADIUS, y); return; }
+        else {
+            int y2 = y + (max-val) * RADIUS * 2 * 2 * 2;
+            printCirclesInLine(val, RADIUS, y);
+            printCirclesInLine(val, RADIUS, y2);
+        }
+        printShape(++val, max, y + RADIUS * 4);
     }
 
     public static void main(String[] args) {
@@ -31,24 +30,29 @@ public class Aufgabe4 {
         StdDraw.setCanvasSize(pixelWidth, pixelHeight);
         StdDraw.setXscale(0, pixelWidth);
         StdDraw.setYscale(0, pixelHeight);
+        StdDraw.enableDoubleBuffering();
 
         //Erzeugt Abbildung 2a
         printShape(1, 10, RADIUS);
+        StdDraw.show();
         StdDraw.pause(3000);
         StdDraw.clear();
 
         //Erzeugt Abbildung 2b
         printShape(1, 5, RADIUS);
+        StdDraw.show();
         StdDraw.pause(3000);
         StdDraw.clear();
 
         //Erzeugt Abbildung 2c
         printShape(1, 1, RADIUS);
+        StdDraw.show();
         StdDraw.pause(3000);
         StdDraw.clear();
 
         //Erzeugt Abbildung 2d
         printShape(3, 9, RADIUS);
+        StdDraw.show();
     }
 }
 
